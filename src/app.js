0966a87292babe68a48db64671db8febc2c12adc
Magnus Sagerup <magnus.sagerup@gmail.com>
2017-10-30 17:01:19 +0100

    continue work on state

--- src/app.js
+++ src/app.js
@@ -11,27 +11,49 @@ class IndecisionApp extends React.Component {
          options: props.options
       }
    }
-  
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
+  }
+
+   componentDidUpdate(prevProps, prevState) {
+      if (prevState.options.length !== this.state.options.length) {
+         const json = JSON.stringify(this.state.options)
+         localStorage.setItem('options', json); 
+      }
+    }
+
+   componentWillUnmount () {
+      console.log('Component unmouted');
+   }
+
+
    handleDeleteOptions() {
       this.setState (() => ({ options: [] }))
    }
 
-   
 
-  handleDeleteOption (option) {
-   console.log('hello', option)
+
+
+  handleDeleteOption (optionToRemove) {
+   this.setState ((prevState) => ({
+      options : prevState.options.filter ((option) => optionToRemove !== option)
+   }));
   }
-
-
-
 
    handlePick() {
       const randomNum = Math.floor(Math.random() * this.state.options.length);
       const option = this.state.options[randomNum];
       alert(option);
    };
-
-
 
    handleAddOption(option) {
       if(!option) {
@@ -148,6 +170,7 @@ const Options = (props) => {
    return (
          <div>
            <button onClick={props.handleDeleteOptions}>Remove All</button>
+           {props.options.length === 0 && <p>Please add an options to get started!</p>}
             {
                props.options.map((option) => (
                   <Option key={option} 
@@ -220,6 +243,11 @@ class AddOption extends React.Component {
       const error = this.props.handleAddOption(option);
 
       this.setState (() => ({ error: error }));
+
+      if (!error) {
+         e.target.elements.option.value = '';
+      }
+
    }
 
    render() {
