5064ce944201ac2fdea4648ff05f2791a7cb12e8
Magnus Sagerup <magnus.sagerup@gmail.com>
2017-11-30 23:05:47 +0100

    added scss support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-
 module.exports = {
   entry: './src/app.js',
   output: {
@@ -12,6 +11,13 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map',
